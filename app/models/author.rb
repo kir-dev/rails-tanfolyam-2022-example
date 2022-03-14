8aa258d38ddac21e79a4dcda452cb736b17c7115
Bucsy Benjámin <bucsy.benjamin@simonyi.bme.hu>
2022-03-14 21:36:18 +0100

    Add author scaffold

---
+++ app/models/author.rb
@@ -0,0 +1,14 @@
+# == Schema Information
+#
+# Table name: authors
+#
+#  id          :integer          not null, primary key
+#  name        :string
+#  birthdate   :date
+#  nationality :string
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
+
+class Author < ApplicationRecord
+end
